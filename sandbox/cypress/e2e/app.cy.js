918e00d90599a07796b22903e9329b20b69893b1
Brent Schmidt <bschmidt@oreilly.com>
2023-07-10 10:24:36 -0400

    fix(codeql): Remove useless escape from test

--- sandbox/cypress/e2e/app.cy.js
+++ sandbox/cypress/e2e/app.cy.js
@@ -24,7 +24,7 @@ describe('local to-do app', () => {
       }
     ).as('static');
 
-    cy.playback('GET', new RegExp('learning\.oreilly')).as('image');
+    cy.playback('GET', new RegExp('learning.oreilly')).as('image');
 
     cy.playback('GET', new RegExp('/todos/'),
       {
